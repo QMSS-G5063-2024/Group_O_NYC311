c841d1ba3d736fa9fb885880082275c89da8195a
Rei Hrn Lim <144067460+T4vish@users.noreply.github.com>
2024-04-30 01:03:17 -0400

    Touching up and adding text to every part of app

--- app.R
+++ app.R
@@ -104,9 +104,11 @@ ui <- fluidPage(
              HTML("<i>From Different Neighborhoods Across The City.</i>"))
   ),
   
-  tags$h2("Overview", style = "text-align: center;"),
+  tags$h2(HTML("<strong><i>Introduction</i></strong>"), 
+          style = "text-align: center;"),
   
-  tags$h5("NYC311 is a platform where residents in New York City can report, inquire, and seek help on a vast array of issues covering areas including but not limited to public transport, environmental concerns, sanitation, and safety issues. Data from all calls made to NYC311 are made publicly available and provide an opportunity to understand what challenges residents of New York City go through on a daily basis. Data from NYC311 can be accessed at https://data.cityofnewyork.us/Social-Services/311-Service-Requests-from-2010-to-Present/erm2-nwe9/about_data.",
+  tags$h5("NYC311 is a platform where residents in New York City can report, inquire, and seek help on a vast array of issues covering areas including but not limited to public transport, environmental concerns, sanitation, and safety issues. Data from all calls made to NYC311 are made publicly available and provide an opportunity to understand what challenges residents of New York City go through on a daily basis. Data from NYC311 can be accessed here:",
+          tags$a("NYC311 Data", href="https://data.cityofnewyork.us/Social-Services/311-Service-Requests-from-2010-to-Present/erm2-nwe9/about_data"),
           style = "text-align: justify;"),
   
   tags$h5("By exploring and visualizing key aspects of the NYC311 dataset, we hope to provide a deeper understanding on the types of issues and concerns that residents have, and also reveal underlying patterns and trends related to the NYC311 calls and their reported issues.",
@@ -115,8 +117,13 @@ ui <- fluidPage(
   tags$h5("Below, we provide several options to visualize the NYC311 data across several dimensions, including at the city-wide level, the borough-level, the community district-level, and at the NYC agency level. Click on the respective tabs below to view visualizations of the data along these dimensions.",
           style = "text-align: justify;"),
   
-  tags$h3("Choose your level of analysis using the various tabs!", class = "text-muted",
+  hr(),
+  
+  tags$h4(HTML("<i>Choose your level of analysis using the various tabs below!</i>"), class = "text-muted",
           style = "text-align: center;"),
+  
+  tags$br(),  # Add an empty line space
+  
   
   tabsetPanel(
     
@@ -138,13 +145,31 @@ ui <- fluidPage(
                                     selected = unique(data$Borough))
                ),
                mainPanel(
-                 h3("Heat Map Of NYC311 Complaints"),
-                 tags$h5("The heat map below displays the concenration and variation in the number of NYC311 call across the entire city. The concentrations range from low (blue) to high (red)."),
+                 h2(HTML("<strong>City-Level Analysis</strong>")),
+                 tags$h4("This tab provides a broad overview of all the NYC311 call complaints in New York City within the month of June 2017. You can use the options in the side panel to select the complaint categories you want to include, as well as which boroughs you want to include in the visualizations below.",
+                         style = "text-align: justify;"),
+                 hr(),
+                 h3(HTML("<strong>Heat Map Of NYC311 Complaints</strong>")),
+                 tags$h4("The heat map below displays the concenration and variation in the number of NYC311 call across the entire city. The concentrations range from low (blue) to high (red).",
+                         style = "text-align: justify;"),
                  leafletOutput("map"),
                  hr(),
-                 h3("An Overview of Complaints"),
+                 h3(HTML("<strong>An Overview of Complaints</strong>")),
+                 tags$h4("Displayed below are visualizations describing trends in NYC311 data for call complaints in June 2017. These visualizations provide an overview of the types of issues New Yorkers encountered, the distribution of NYC311 calls across the five boroughts, as well as temporal trends in NYC311 call numbers. The original data contained 187 different unique complaint types which we have grouped into these 7 broad categories.",
+                         style = "text-align: justify;"),
+                 tags$br(),
+                 tags$h4("The graph below shows the breakdown of NYC311 calls by their complaint categories. The most frequently encountered type of issue by New Yorkers was noise-related incidences and disriptions. Meanwhile, the least frequently reported type of complaints were those related to safety and security concerns.",
+                         style = "text-align: justify;"),
                  plotlyOutput("type_plot"),
+                 tags$br(),
+                 tags$h4("The graph below shows the distribution of NYC311 call numbers across the five boroughs in New York City. We can see that the largest number of NYC311 calls were made in Brooklyn, while Staten Island had the lowest number of NYC311 calls.",
+                         style = "text-align: justify;"),
+                 tags$br(),
                  plotlyOutput("borough_plot"),
+                 tags$br(),
+                 tags$h4("The graph below shows the temporal trends in the total number of NYC311 calls in the month of June 2017.",
+                         style = "text-align: justify;"),
+                 tags$br(),
                  plotlyOutput("time_plot")
                )
              )
@@ -165,18 +190,38 @@ ui <- fluidPage(
                                     selected = "Noise-related complaints")
                ),
                mainPanel(
-                 uiOutput("maps")
+                 h2(HTML("<strong>Borough-Level Cluster Map Of NYC311 Calls</strong>")),
+                 tags$h4("Moving on from the city-level analysis, this tab presents cluster maps to visualize the geographical concentrations and locations of NYC311 calls within each of the five boroughs in New York City.",
+                         style = "text-align: justify;"),
+                 hr(),
+                 tags$h4("Use the sub-tabs below to select the borough that you wish to explore. You can also select which kind of NYC311 issues and complaints you want to include in your exploration in the side bar on the left.",
+                         style = "text-align: justify;"),
+                 uiOutput("maps"),
+                 tags$br(),
+                 tags$br(),
+                 tags$br(),
+                 tags$br()
                )
              )
     ),
     
     tabPanel("NYC Community Districts Level", 
              fluidRow(
-               column(12, selectInput("CD_bar_borough", "Select Borough",
+               h2(HTML("<strong>Community District-Level Analysis</strong>")),
+               h4("New York City comprises of 59 Community Districts, each of them containing different neighborhoods in the city. This tab provides details of NYC311 calls at the Community-District Level and is especially useful for understanding variations in NYC311 issues encountered by residents across the different neighborhoods.",
+                  style = "text-align: justify;"),
+               hr(),
+               h4("This segmented bar chart shows the variation in the total number of NYC311 complaints in each Community Distirct, as well as the breakdown of NYC311 calls by complaint categoreis within each Community District. Use the dropdown menu below to view the information on the Community Districts within each borough."),
+               br(),
+               column(12, selectInput("CD_bar_borough", "Select The Borough You Want To Explore",
                                       choices = unique(data$Borough),
                                       selected = "Manhattan")
                ),
                column(12, plotlyOutput("CD_Manhattan")),
+               h4("     "),
+               h4("     "),
+               h4("This interactive datable below displays the borough and neighborhoods contained within each of the 59 Community Districts in NYC and reports the number of NYC311 calls under each complaint category. You can sort and filter the results by any of the columns. If you want to view details on a specific neighborhood, you can enter its name in the filter search bar below."),
+               br(),
                column(12,dataTableOutput("CD_NYC311_table"))
              )
     ),
@@ -190,11 +235,25 @@ ui <- fluidPage(
                  dateRangeInput("dateRange", "Select Date Range:", start = min(data$Created.Date, na.rm = TRUE), end = max(data$Created.Date, na.rm = TRUE))
                ),
                mainPanel(
-                 h3("Section 1: Overview of Complaints and Response Times by Agency"),
+                 h2(HTML("<strong>Analysing NYC311 Calls By Government Agency</strong>")),
+                 h4("On this tab, you can explore the types and volumes of NYC311 complaints that each agency responded to in June 2017. This provides a deeper understanding of how government agencies are responding to the issues encountered by residents in New York City.",
+                    style = "text-align: justify;"),
+                 h4("Select which agency you would like to explore using the dropdown menu in the side panel. You can also filter further by complaint category as well as a specific subset of dates within June 2017.",
+                    style = "text-align: justify;"),
+                 hr(),
+                 h3(HTML("<strong>Overview of Complaints and Response Times by Agency</strong>")),
+                 h4("The multi-layered graph below displaus the number and type of NYC311 complaints that agencies responded to."),
                  plotlyOutput("agency_timeSeriesPlot"),
+                 h4("The graph below shows the breakdown of average response time in hours that the agency spent to respond to each type of complaint."),
                  plotlyOutput("agency_responseTimePlot"),
-                 h3("Section 2: Most Commonly Used Words received in Complaints by Agency"),
+                 hr(),
+                 tags$br(),
+                 h3(HTML("<strong>Text Analysis Of Most Commonly Words In NYC311 Complaints By Agency</strong>")),
+                 tags$br(),
+                 h4("The word cloud below displays the most commonly used words within the pool of complaints encountered by each agency. This provides an idea of what specific issues and problems the NYC311 calls pertain to."),
                  plotOutput("agency_wordCloud"),
+                 h4("The graph below displays the frequency counts of the 30 most common words within the pool of NYC311 call complaints each agency received. This gives us more information on the nature of concerns that residents in New York have, which each agency oversees and responds to."),
+                 tags$br(),
                  plotlyOutput("agency_wordFreqPlot"))
              )
     )
@@ -261,9 +320,9 @@ server <- function(input, output) {
   output$CD_Manhattan = renderPlotly({
     ggplot(CD_Manhattan_data(), aes(x = Community.Board, fill = Complaint.Category)) +
       geom_bar(position = "stack") +
-      labs(title = "Breakdown Of NYC311 By Community District",
-           x = "Community Districts",
-           y = "Number of NYC311 Calls") +
+      labs(title = "Breakdown Of NYC311 Call Numbers By Community District",
+           x = NULL,
+           y = NULL) +
       scale_fill_manual(values = c("Noise-related complaints" = "orange1", 
                                    "Transportation problems" = "dodgerblue",
                                    "Environmental concerns" = "lawngreen",
@@ -391,7 +450,7 @@ server <- function(input, output) {
     title_text <- sprintf("%s Total Number of Complaints per Month", input$selectedAgency)
     p <- ggplot(agency_filtered_data(), aes(x = Created.Date, fill = Complaint.Type)) +
       geom_histogram(binwidth = 30, position = "identity", alpha = 0.5) +
-      labs(title = title_text, x = "Date", y = "Count of Complaints") +
+      labs(title = title_text, x = "Date", y = NULL) +
       theme_minimal()
     ggplotly(p)
   })
@@ -409,7 +468,7 @@ server <- function(input, output) {
     } else {
       print("No words to display in word cloud.")
     }
-  })
+  }) 
   
   output$agency_wordFreqPlot <- renderPlotly({
     data <- agency_filtered_data()
@@ -418,12 +477,11 @@ server <- function(input, output) {
       unnest_tokens(word, Descriptor) %>%
       anti_join(stop_words, by = "word") %>%
       count(word, sort = TRUE) %>%
-      top_n(50, n)
-    p <- ggplot(words, aes(x = reorder(word, n), y = n)) +
+      top_n(30, n)
+    p <- ggplot(words, aes(y = reorder(word, n), x = n)) +
       geom_col() +
-      labs(title = sprintf("Top 50 most common words received in complaints by %s", input$selectedAgency), x = "Words", y = "Frequency") +
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+      labs(title = sprintf("Top 30 most common words received in complaints by %s", input$selectedAgency), x = "Words", y = "Frequency") +
+      theme_minimal() 
     ggplotly(p)
   })
   
@@ -434,11 +492,10 @@ server <- function(input, output) {
       summarise(AverageResponse = mean(Response.Times, na.rm = TRUE)) %>%
       ungroup() %>%
       arrange(desc(AverageResponse))
-    title_text <- sprintf(" Average Response Time by Complaint Type", input$AgencycomplaintCategory)
-    p <- ggplot(df, aes(x = reorder(Complaint.Type, -AverageResponse), y = AverageResponse)) +
+    title_text <- sprintf(" Average Response Time (Hours) By Complaint Type", input$AgencycomplaintCategory)
+    p <- ggplot(df, aes(y = reorder(Complaint.Type, -AverageResponse), x = AverageResponse)) +
       geom_bar(stat = "identity", fill = "dodgerblue") +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-      labs(title = title_text, x = "Complaint Type", y = "Average Response Time (hours)")
+      labs(title = title_text, y = NULL, x = NULL)
     ggplotly(p)
   })
   
